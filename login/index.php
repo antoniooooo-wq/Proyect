d1f04cd5aca6dc90f0cbdcd60cee528b8d5c3c16
Ricardo <atoxapk.2015@gmail.com>
2022-06-13 00:32:30 -0400

    login

--- login/index.php
+++ login/index.php
@@ -48,7 +48,7 @@
 							</div>
 
 							<div class="wrap-input100 validate-input" data-validate="Contraseña Requerida">
-								<input class="input100" type="password" id="frmclave" name="pass" ame="frmclave" placeholder="Contraseña">
+								<input class="input100" type="password" id="frmclave" name="frmclave" placeholder="Contraseña">
 								<span class="focus-input100"></span>
 								<span class="symbol-input100">
 									<i class="fa fa-lock" aria-hidden="true"></i>
